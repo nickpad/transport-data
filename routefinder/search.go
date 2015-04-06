88a3ea88d03070df633563b0b17e163e504e8b02
Nick Dainty <nickdainty@gmail.com>
2015-04-06 19:01:15 +1000

    Extract initPriorityQueue method

--- routefinder/search.go
+++ routefinder/search.go
@@ -8,78 +8,39 @@ import (
 // The maximum distance a vertex can be from the start vertex.
 const maxDistance int64 = math.MaxInt64
 
-// State stores the current state of a search.
-type State struct {
-	graph         Graph             // The graph to search
-	start         *Vertex           // The vertex to search from
-	end           *Vertex           // The vertex to search to
-	departAt      int64             // The departure time
-	priorityQueue *PriorityQueue    // Used to determine next vertex to explore
-	distances     map[*Vertex]int64 // Current distances for each vertex
-	predecessors  map[*Vertex]*Edge // Predecessor edge used to reach each vertex
-	itemMap       map[*Vertex]*Item // Maps vertices to priorityQueue Items
-}
+type (
+	distanceMap   map[*Vertex]int64
+	breadcrumbMap map[*Vertex]*Edge
+	pqItemMap     map[*Vertex]*Item
+
+	// State stores the current state of a search.
+	State struct {
+		graph       Graph          // The graph to search
+		start       *Vertex        // The vertex to search from
+		end         *Vertex        // The vertex to search to
+		departAt    int64          // The departure time
+		pq          *PriorityQueue // Used to determine next vertex to explore
+		distances   distanceMap    // Current distances for each vertex
+		breadcrumbs breadcrumbMap  // Predecessor edge used to reach each vertex
+		pqItems     pqItemMap      // Maps vertices to priorityQueue Items
+	}
+)
 
 // NewState initializes a new State instance.
 func NewState(graph Graph, start, end *Vertex, departAt int64) *State {
-	pq := make(PriorityQueue, len(graph))
-
 	state := State{
-		graph:         graph,
-		start:         start,
-		end:           end,
-		departAt:      departAt,
-		priorityQueue: &pq,
-		distances:     map[*Vertex]int64{},
-		predecessors:  map[*Vertex]*Edge{},
-		itemMap:       map[*Vertex]*Item{},
+		graph:       graph,
+		start:       start,
+		end:         end,
+		departAt:    departAt,
+		distances:   distanceMap{},
+		breadcrumbs: breadcrumbMap{},
+		pqItems:     pqItemMap{},
 	}
 
-	i := 0
-	for _, vtx := range graph {
-		pqItem := &Item{
-			value:    vtx,
-			priority: maxDistance - state.getDistance(vtx),
-			index:    i,
-		}
-
-		pq[i] = pqItem
-		state.itemMap[vtx] = pqItem
-		i++
-	}
-
-	heap.Init(&pq)
+	state.initPriorityQueue()
 
 	return &state
-}
-
-func (state *State) nextVertex() *Vertex {
-	if len(*state.priorityQueue) > 0 {
-		item := heap.Pop(state.priorityQueue).(*Item)
-		return item.value
-	}
-	return nil
-}
-
-func (state *State) getDistance(vertex *Vertex) int64 {
-	if vertex == state.start {
-		return 0
-	}
-
-	distance, ok := state.distances[vertex]
-
-	if ok {
-		return distance
-	}
-
-	return maxDistance
-}
-
-func (state *State) increasePriority(vertex *Vertex, amount int64) {
-	item, ok := state.itemMap[vertex]
-	if ok {
-		state.priorityQueue.IncreasePriority(item, amount)
-	}
 }
 
 // Search performs a shortest-path search over the State graph.
@@ -101,7 +62,7 @@ func (state *State) Search() {
 			newDistance := currentDistance + edge.Weight()
 			if newDistance < successorDistance {
 				state.distances[successor] = newDistance
-				state.predecessors[successor] = edge
+				state.breadcrumbs[successor] = edge
 				state.increasePriority(successor, newDistance)
 				currentTime = edge.Arrives
 			}
@@ -119,7 +80,7 @@ func (state *State) Path() Path {
 	current := state.end
 
 	for {
-		predecessor, ok := state.predecessors[current]
+		predecessor, ok := state.breadcrumbs[current]
 
 		if ok {
 			path = append([]Edge{*predecessor}, path...)
@@ -130,4 +91,53 @@ func (state *State) Path() Path {
 	}
 
 	return path
+}
+
+func (state *State) getDistance(vertex *Vertex) int64 {
+	if vertex == state.start {
+		return 0
+	}
+
+	distance, ok := state.distances[vertex]
+
+	if ok {
+		return distance
+	}
+
+	return maxDistance
+}
+
+func (state *State) increasePriority(vertex *Vertex, amount int64) {
+	item, ok := state.pqItems[vertex]
+	if ok {
+		state.pq.IncreasePriority(item, amount)
+	}
+}
+
+func (state *State) initPriorityQueue() {
+	pq := make(PriorityQueue, len(state.graph))
+
+	i := 0
+	for _, vtx := range state.graph {
+		pqItem := &Item{
+			value:    vtx,
+			priority: maxDistance - state.getDistance(vtx),
+			index:    i,
+		}
+
+		pq[i] = pqItem
+		state.pqItems[vtx] = pqItem
+		i++
+	}
+
+	heap.Init(&pq)
+	state.pq = &pq
+}
+
+func (state *State) nextVertex() *Vertex {
+	if len(*state.pq) > 0 {
+		item := heap.Pop(state.pq).(*Item)
+		return item.value
+	}
+	return nil
 }
